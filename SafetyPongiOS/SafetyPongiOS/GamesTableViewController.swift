77fbd5318b1ba6eac7ad4f3b98f13c656b85e64a
SafetyMarcus <hooperarma@gmail.com>
2016-04-12 22:32:17 +1000

    Setting up tables and tabs

---
+++ SafetyPongiOS/SafetyPongiOS/GamesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GamesTableViewController.swift
+//  SafetyPongiOS
+//
+//  Created by Marcus Hooper on 12/04/2016.
+//  Copyright © 2016 HoopRay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GamesTableViewController: UITableViewController
+{
+    @IBOutlet weak var gamesTable: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ SafetyPongiOS/SafetyPongiOS/PlayersTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlayersTableViewController.swift
+//  SafetyPongiOS
+//
+//  Created by Marcus Hooper on 12/04/2016.
+//  Copyright © 2016 HoopRay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayersTableViewController: UITableViewController
+{
+    @IBOutlet weak var playersTable: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
+
